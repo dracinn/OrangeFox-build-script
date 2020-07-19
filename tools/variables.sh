07cfafc9e58fe21a1cb8ae9df8f77b4444bbe36b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-19 02:42:00 +0200

    variables: Add common R11 variables

--- tools/variables.sh
+++ tools/variables.sh
@@ -10,3 +10,7 @@ export LC_ALL="C"
 export USE_CCACHE="1"
 # Modify this variable to your name
 export OF_MAINTAINER="SebaUbuntu"
+# Enforced by R11 rules
+export FOX_R11="1"
+export FOX_ADVANCED_SECURITY="1"
+export FOX_RESET_SETTINGS="1"
