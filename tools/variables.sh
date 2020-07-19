caf59b5f90ca5f5c8f96fd3a8cbe66c1539df5e8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-07-19 02:38:08 +0200

    variables: Cleanup

--- tools/variables.sh
+++ tools/variables.sh
@@ -9,14 +9,6 @@ export TW_DEFAULT_LANGUAGE="en"
 export LC_ALL="C"
 # To use ccache to speed up building
 export USE_CCACHE="1"
-# Prevent issues like bootloop on encrypted devices
-export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-# Try to decrypt data when a MIUI backup is restored
-export OF_OTA_RES_DECRYPT="1"
-# Include full bash shell
-export FOX_USE_BASH_SHELL="1"
-# Include nano editor
-export FOX_USE_NANO_EDITOR="1"
 # Modify this variable to your name
 export OF_MAINTAINER="SebaUbuntu"
 }
